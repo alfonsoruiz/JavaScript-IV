ef782f161149b938ffcb87a1a118059bb0f4ab4f
Alfonso Ruiz <1800ruiz@gmail.com>
2019-04-04 20:34:24 -0700

    finished lambda-classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -52,7 +52,7 @@ class ProjectManager extends Instructor {
         this.favInstructor = attr.favInstructor;
     }
     standUp(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+        console.log(`${this.name} announces to @${channel} standy times!`);
     }
     debugsCode(student, subject) {
         console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
@@ -69,16 +69,82 @@ const fred = new Instructor({
     catchPhrase: `Don't forget the homies`
 });
 
+const fred2 = new Instructor({
+    name: 'Fred 2',
+    location: 'Bedrock 2',
+    age: 38,
+    gender: 'F',
+    favLanguage: 'JavaScript 2',
+    specialty: 'Front-end 2',
+    catchPhrase: `Don't forget the homies 2`
+});
+
 const rupert = new Student({
     name: 'Rupert',
     location: 'Everywhere',
     gender: 'M',
     previousBackground: 'Kiky',
     className: 'Rupert 101',
-    favSubjects: 'Treats'
+    favSubjects: ['Treats', 'Smacking']
+});
+
+const rupert2 = new Student({
+    name: 'Rupert2',
+    location: 'Everywhere2',
+    gender: 'F',
+    previousBackground: 'Kiky2',
+    className: 'Rupert 102',
+    favSubjects: ['Treats2', 'Smacking2']
+});
+
+const rene = new ProjectManager({
+    name: 'Rene',
+    location: 'Irvine',
+    age: 37,
+    gender: 'M',
+    favLanguage: 'C++',
+    specialty: 'Architecture',
+    catchPhrase: 'Are you done yet',
+    gradClassName: 'CS2K19',
+    favInstructor: 'Cam'
+});
+
+const rene2 = new ProjectManager({
+    name: 'Rene2',
+    location: 'Costa Mesa',
+    age: 38,
+    gender: 'F',
+    favLanguage: 'C#',
+    specialty: 'Architecture 2',
+    catchPhrase: 'Are you done yet 2',
+    gradClassName: 'CS2K20',
+    favInstructor: 'Cam2'
 });
 
 console.log(`========= lambda-Classes =========`);
+console.log(`Instructor:`)
 console.log(fred.speak());
-console.log(fred.demo('Ojects'));
+console.log(fred.demo('Obects'));
+console.log(fred2.speak());
+console.log(fred2.demo('Obects 2'));
+console.log(`Student:`);
+console.log(rupert.speak());
+console.log(rupert.listSubjects());
+console.log(rupert.PRAssignment(`Black cat white cat`));
+console.log(rupert.sprintChallenge('crying'));
+console.log(rupert2.speak());
+console.log(rupert2.listSubjects());
+console.log(rupert2.PRAssignment(`Black cat white cat 2`));
+console.log(rupert2.sprintChallenge('crying 2'));
+console.log(`ProjectManager:`);
+console.log(rene.speak())
+console.log(rene.standUp('class'));
+console.log(rene.debugsCode(rupert, 'Crying for treats'));
+console.log(rene.gradClassName);
+console.log(rene.favInstructor);
+console.log(rene2.speak());
+console.log(rene2.standUp('class2'));
+console.log(rene2.debugsCode(rupert2, 'Crying for MORE treats'));
+console.log(rene2.gradClassName);
+console.log(rene.favInstructor);
 
