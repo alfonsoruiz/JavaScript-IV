7ba760a7d55019ff395204afba50e32e284d4b22
Alfonso Ruiz <1800ruiz@gmail.com>
2019-04-04 20:09:53 -0700

    completed prototype-refactor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,84 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attr) {
+        this.name = attr.name;
+        this.location = attr.location;
+        this.gender = attr.gender;
+    }
+    speak() {
+        console.log(`Hello my name is ${this.name} I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(attr) {
+        super(attr);
+        this.specialty = attr.specialty;
+        this.favLanguage = attr.favLanguage;
+        this.catchPhrase = attr.catchPhrase;
+    }
+    demo(subject) {
+       console.log(`Today we are learning about ${subject}`);
+    }
+    grade(studentObj, subject) {
+        console.log(`${studentObj.name} recieves a perfect score on ${subject}`);
+    }
+}
+
+class Student extends Person {
+    constructor(attr) {
+        super(attr);
+        this.previousBackground = attr.previousBackground;
+        this.className = attr.className;
+        this.favSubjects = attr.favSubjects;
+    }
+    listSubjects() {
+        this.favSubjects.forEach((element) => {
+            console.log(element)
+        });
+    }
+    PRAssignment(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    }
+}
+
+class ProjectManager extends Instructor {
+    constructor(attr) {
+        super(attr);
+        this.gradClassName = attr.gradClassName;
+        this.favInstructor = attr.favInstructor;
+    }
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    }
+}
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+});
+
+const rupert = new Student({
+    name: 'Rupert',
+    location: 'Everywhere',
+    gender: 'M',
+    previousBackground: 'Kiky',
+    className: 'Rupert 101',
+    favSubjects: 'Treats'
+});
+
+console.log(`========= lambda-Classes =========`);
+console.log(fred.speak());
+console.log(fred.demo('Ojects'));
+
